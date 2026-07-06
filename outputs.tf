e0d5c248da4af80afd40ade914c3cccbae5a41e2
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:10:45 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,28 @@ output "api_connections" {
   description = "All api_connection resources"
   value       = azurerm_api_connection.api_connections
 }
+output "api_connections_display_name" {
+  description = "List of display_name values across all api_connections"
+  value       = [for k, v in azurerm_api_connection.api_connections : v.display_name]
+}
+output "api_connections_managed_api_id" {
+  description = "List of managed_api_id values across all api_connections"
+  value       = [for k, v in azurerm_api_connection.api_connections : v.managed_api_id]
+}
+output "api_connections_name" {
+  description = "List of name values across all api_connections"
+  value       = [for k, v in azurerm_api_connection.api_connections : v.name]
+}
+output "api_connections_parameter_values" {
+  description = "List of parameter_values values across all api_connections"
+  value       = [for k, v in azurerm_api_connection.api_connections : v.parameter_values]
+}
+output "api_connections_resource_group_name" {
+  description = "List of resource_group_name values across all api_connections"
+  value       = [for k, v in azurerm_api_connection.api_connections : v.resource_group_name]
+}
+output "api_connections_tags" {
+  description = "List of tags values across all api_connections"
+  value       = [for k, v in azurerm_api_connection.api_connections : v.tags]
+}
 
