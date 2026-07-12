b593c34bcb2b2f0ae7c48bc73792ddd71fa24b11
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:21 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "api_connections" {
-  description = "All api_connection resources"
-  value       = azurerm_api_connection.api_connections
-}
 output "api_connections_display_name" {
-  description = "List of display_name values across all api_connections"
-  value       = [for k, v in azurerm_api_connection.api_connections : v.display_name]
+  description = "Map of display_name values across all api_connections, keyed the same as var.api_connections"
+  value       = { for k, v in azurerm_api_connection.api_connections : k => v.display_name }
 }
 output "api_connections_managed_api_id" {
-  description = "List of managed_api_id values across all api_connections"
-  value       = [for k, v in azurerm_api_connection.api_connections : v.managed_api_id]
+  description = "Map of managed_api_id values across all api_connections, keyed the same as var.api_connections"
+  value       = { for k, v in azurerm_api_connection.api_connections : k => v.managed_api_id }
 }
 output "api_connections_name" {
-  description = "List of name values across all api_connections"
-  value       = [for k, v in azurerm_api_connection.api_connections : v.name]
+  description = "Map of name values across all api_connections, keyed the same as var.api_connections"
+  value       = { for k, v in azurerm_api_connection.api_connections : k => v.name }
 }
 output "api_connections_parameter_values" {
-  description = "List of parameter_values values across all api_connections"
-  value       = [for k, v in azurerm_api_connection.api_connections : v.parameter_values]
+  description = "Map of parameter_values values across all api_connections, keyed the same as var.api_connections"
+  value       = { for k, v in azurerm_api_connection.api_connections : k => v.parameter_values }
 }
 output "api_connections_resource_group_name" {
-  description = "List of resource_group_name values across all api_connections"
-  value       = [for k, v in azurerm_api_connection.api_connections : v.resource_group_name]
+  description = "Map of resource_group_name values across all api_connections, keyed the same as var.api_connections"
+  value       = { for k, v in azurerm_api_connection.api_connections : k => v.resource_group_name }
 }
 output "api_connections_tags" {
-  description = "List of tags values across all api_connections"
-  value       = [for k, v in azurerm_api_connection.api_connections : v.tags]
+  description = "Map of tags values across all api_connections, keyed the same as var.api_connections"
+  value       = { for k, v in azurerm_api_connection.api_connections : k => v.tags }
 }
 
