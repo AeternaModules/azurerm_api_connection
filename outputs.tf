312f06337b4e5e31cf5a314a1c3abfbed9c8f777
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:00:03 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "api_connections_id" {
+  description = "Map of id values across all api_connections, keyed the same as var.api_connections"
+  value       = { for k, v in azurerm_api_connection.api_connections : k => v.id }
+}
 output "api_connections_display_name" {
   description = "Map of display_name values across all api_connections, keyed the same as var.api_connections"
   value       = { for k, v in azurerm_api_connection.api_connections : k => v.display_name }
